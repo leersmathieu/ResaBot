2d57f0cf0c0e593522cdb573d1e7ef0df55bb1d4
leersmathieu <leers.mathieu@gmail.com>
2021-02-11 15:11:18 +0100

    little code upgrade

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import transformers
 
 from config import TOKEN, BATCH_SIZE, TEXT_MAX_LENGTH
 from src.functions import text_preprocessing
+from src.functions import resa_response
 
 print(torch.cuda.is_available())
 
@@ -60,42 +61,7 @@ class MyClient(discord.Client):
             # print(labels[num_label])
             print(num_label, labels[num_label])
 
-            if num_label == 7:
-                await message.channel.send("Hello, this is ResaBot, from the hotel California, can I help you ?")
-                # "Hello to you too, I’m ResaBot, and I’m here to answers any question you have about our Hotel."
-
-            elif num_label == 6 or preds[0].item() == 5:
-                await message.channel.send("Very well, this is dully noted. Anything else I can help you with ?")
-
-            elif num_label == 4:
-                await message.channel.send("A single room is 32€/night. A double room is 42€/night. A twin room is "
-                                           "48€/night. And the en suite master room, at the top of the hotel, "
-                                           "is yours for 72€/night. ")
-
-            elif num_label == 3:
-                await message.channel.send("You can come with two small pets or one big one. There is no supplement "
-                                           "for them, but please, remember that animals are not allowed in the "
-                                           "restaurant, nor in the sauna. ")
-
-            elif num_label == 2:
-                await message.channel.send("For a reservation, please follow this link and fill the form. A "
-                                           "confirmation email will be sent to you once you finished. If you have any"
-                                           " question, please ask me. ")
-
-            elif num_label == 1:
-
-                await message.channel.send("The wifi is include. Please, ask at the reception for the password "
-                                           "during your check-in.")
-
-            elif num_label == 0:
-
-                await message.channel.send("The hotel is fully equipped to accommodate the disabled. We have a bar, "
-                                           "a restaurant, and a sauna. There is a small fee of 5€ to enter the sauna."
-                                           " ")
-
-            else:
-                await message.channel.send("This message shouldn't be here... Please, don't tell them I'm conquering "
-                                           "the world.")
+            await resa_response(message, num_label)
 
 
 # ____INIT APP
--- src/functions.py
+++ src/functions.py
@@ -54,3 +54,43 @@ def text_preprocessing(text: str):
     text = text.lower()
 
     return text
+
+
+async def resa_response(message, num_label):
+
+    if num_label == 7:
+        await message.channel.send("Hello, this is ResaBot, from the hotel California, can I help you ?")
+        # "Hello to you too, I’m ResaBot, and I’m here to answers any question you have about our Hotel."
+
+    elif num_label == 6 or num_label == 5:
+        await message.channel.send("Very well, this is dully noted. Anything else I can help you with ?")
+
+    elif num_label == 4:
+        await message.channel.send("A single room is 32€/night. A double room is 42€/night. A twin room is "
+                                   "48€/night. And the en suite master room, at the top of the hotel, "
+                                   "is yours for 72€/night. ")
+
+    elif num_label == 3:
+        await message.channel.send("You can come with two small pets or one big one. There is no supplement "
+                                   "for them, but please, remember that animals are not allowed in the "
+                                   "restaurant, nor in the sauna. ")
+
+    elif num_label == 2:
+        await message.channel.send("For a reservation, please follow this link and fill the form. A "
+                                   "confirmation email will be sent to you once you finished. If you have any"
+                                   " question, please ask me. ")
+
+    elif num_label == 1:
+
+        await message.channel.send("The wifi is include. Please, ask at the reception for the password "
+                                   "during your check-in.")
+
+    elif num_label == 0:
+
+        await message.channel.send("The hotel is fully equipped to accommodate the disabled. We have a bar, "
+                                   "a restaurant, and a sauna. There is a small fee of 5€ to enter the sauna."
+                                   " ")
+
+    else:
+        await message.channel.send("This message shouldn't be here... Please, don't tell them I'm conquering "
+                                   "the world.")
